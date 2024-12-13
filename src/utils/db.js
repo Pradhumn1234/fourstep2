606d8cd7f9f8d32992f2557c9632e08bd93ae313
Pradh123 <vermapradhumn3@gmail.com>
2024-12-13 17:55:48 +0530

    pradhumn verma correction in mongodb

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,25 +1,22 @@
 // src/utils/mongodb.js
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-let isConnected = false; // Track the connection status
+let isConnected = false; // Track connection state
 
 const dbConnect = async () => {
   if (isConnected) {
-    console.log("Already connected to MongoDB");
     return;
   }
-  try {
-    const db = await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
 
+  try {
+    const uri = process.env.MONGODB_URI;
+    const db = await mongoose.connect(uri, { dbName: "foursteps1" });
     isConnected = db.connections[0].readyState === 1;
     console.log("Connected to MongoDB");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error("MongoDB connection error:", error);
+    throw new Error("Failed to connect to MongoDB");
   }
 };
+
 export default dbConnect;
